70267f0585705d7f4ce2f723dafe3c65a8a4c802
igbucur <ioan.gabriel.bucur@gmail.com>
2021-09-21 13:11:06 +0200

    added extra documentation needed to make the Rcpp functions work

---
+++ R/autoRasch.R
@@ -0,0 +1,8 @@
+#' autoRasch: A package for semi-automatic Rasch analysis
+#' 
+#' @docType package
+#' @name autoRasch
+#' @useDynLib autoRasch, .registration=TRUE
+#' @exportPattern("^[[:alpha:]]+")
+#' @importFrom Rcpp evalCpp
+NULL
